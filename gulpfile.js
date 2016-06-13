e465a6d4fed73d7f01bd1505ec4e47166ae79752
Petr Glinka <glinkap@gmail.com>
2016-06-13 22:29:50 +0300

    second

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,40 @@
 var gulp  			= require('gulp'),
 	sass 			= require('gulp-sass'),
-	browserSync 	= require('browser-sync');
+	browserSync 	= require('browser-sync'),
+	concat 			= require('gulp-concat'),
+	uglify			= require('gulp-uglifyjs');
 
 gulp.task('sass', function(){
 	return gulp.src('app/sass/*.+(scss|sass)')
 		.pipe(sass())
 		.pipe(gulp.dest('app/css'))
+		.pipe(browserSync.reload({stream: true}))
 	});
 
-gulp.task('watch', function(){
-	gulp.watch('app/sass/*.+(scss|sass)',['sass']); // при изменение файлов запускать такс sass
+gulp.task('scripts', function(){
+	return gulp.src([
+		'app/vendor/jquery/dist/jquery.min.js'
+		])
+		.pipe(concat('vendor.min.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('app/js'))
+		;
 	});
 
 gulp.task('browser-sync', function(){
 	browserSync({
 		server: {
 			baseDir: 'app'
-		}
+		},
+		notify: false
 
 		});
 	});
+
+gulp.task('watch',['browser-sync','sass'], function(){
+	gulp.watch('app/sass/*.+(scss|sass)',['sass']); // при изменение файлов запускать такс sass
+	gulp.watch('app/**/*.+(html|shtml)', browserSync.reload);
+	gulp.watch('app/js/*.js', browserSync.reload);
+	});
+
+
